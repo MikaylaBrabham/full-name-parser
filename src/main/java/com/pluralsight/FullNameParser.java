31fe2aa4e7b4a65a4f0a035b068d0bf513536129
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-26 18:56:38 -0500

    gather the user first middle and last anme

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
+// import scanner
+import java.util.Scanner;
 
 public class FullNameParser {
+    public static void main(String[] args) {
+        // enter my scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    public static String getName(Scanner scanner) {
+        // get the users first name and get variable
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.nextLine().trim();
+
+        // get the users middle name
+        System.out.println("Enter your middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+        // get the users last name
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine().trim();
 }
