9a492faacdb1f91ad87b53ab31f1228aba27fe04
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-26 23:25:21 -0500

    name parts and name

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -10,7 +10,6 @@ public class FullNameParser {
         // call the getName method
         String fullNamePar = getName(scanner);
         System.out.println("Full Name: " + fullNamePar);
-
     }
 
     public static String getName(Scanner scanner) {
@@ -18,24 +17,36 @@ public class FullNameParser {
         System.out.println(" Please enter your name:");
         String[] fullName = scanner.nextLine().trim().split(" ");
 
+
         // firstName variable
         //String firstName = fullName[0];
 
         //print the first and middle name and last name
         String fullNamePar = "Full Name: ";
         if (fullName.length == 3) {
+            //assign name parts to variables
             String firstName = fullName[0];
             String middleName = fullName[1];
             String lastName = fullName[2];
-            //fulls ente
 
+            //print out full name and parts of name
             fullNamePar = firstName + " " + middleName + " " + lastName;
+            System.out.println("First Name: " + firstName);
+            System.out.println("Middle Name: " + middleName);
+            System.out.println("Last Name: " + lastName);
+
         }
         if (fullName.length == 2) {
+            //assign name parts to variables
             String firstName = fullName[0];
             String noMiddle = "(none)" + " ";
             String lastName = fullName[1];
+
+            //print out full name and parts of name
             fullNamePar = firstName + " " + noMiddle + " " + lastName;
+            System.out.println("First Name: " + firstName);
+            System.out.println("Middle Name: " + noMiddle);
+            System.out.println("Last Name: " + lastName);
         }
         return fullNamePar;
     }
